e430f92104470916effce723ed1e5c7ec2731d7e
Francy Lang <francy.lang@gmail.com>
2017-12-11 16:45:06 -0700

    Edit server

--- server.js
+++ server.js
@@ -18,7 +18,6 @@ if (process.env.NODE_ENV === 'production') { app.use(httpsRedirect); }
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Build Your Own Backend';
 
-
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}`);
 })
