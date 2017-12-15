38ae8958c9871d95ed7c35c1639c4700528bd0da
Francy Lang <francy.lang@gmail.com>
2017-12-14 18:20:04 -0700

    Pass get test for games/:id endpoint

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -45,6 +45,8 @@ describe('API routes', () => {
           response.body.includes('Mega Man X')
           response.body[0].should.have.property('game_image');
           response.body.includes('https://www.speedrun.com/themes/smo/cover-256.png')
+          response.body[0].should.have.property('created_at');
+          response.body[0].should.have.property('updated_at');
       });
     });
 
@@ -75,6 +77,8 @@ describe('API routes', () => {
           response.body.includes('3h 23m 33s')
           response.body[0].should.have.property('game_id');
           response.body.includes('10')
+          response.body[0].should.have.property('created_at');
+          response.body[0].should.have.property('updated_at');
         });
         done();
     });
@@ -89,11 +93,47 @@ describe('API routes', () => {
     });
   });
 
-  describe('GET /api/vi/records/:id', () => {
-    it('should retrieve a record based on the id', () => {
+  describe('GET /api/v1/records/:id', () => {
+    it('should retrieve a record based on the id', (done) => {
       chai.request(server)
-        .get('/api/v')
+        .get('/api/v1/records/:id')
+        .then((response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(1);
+          response.body[0].should.have.property('handle');
+          response.body.includes({ handle: 'Dezanig' });
+          response.body[0].should.have.property('rank');
+          response.body.includes({ rank: '7th' });
+          response.body[0].should.have.property('time');
+          response.body.includes({ time: '1h 41m 41s' });
+          response.body[0].should.have.property('game_id')
+          response.body.includes({ game_id: 2 });
+          response.body[0].should.have.property('created_at');
+          response.body.includes({ created_at: '2017-12-12T22:08:49.706Z' });
+          response.body[0].should.have.property('updated_at');
+          response.body.includes({ created_at: '2017-12-12T22:08:49.706Z' });
+        })
+        done();
+      })
     })
 
-  });
+  describe('GET /api/v1/games/:id', () => {
+    it('should retrieve a game based on the id', (done) => {
+      chai.request(server)
+        .get('/api/v1/games/:id')
+        .then((response) => {
+          response.should.have.status(200);
+          response.should.be.json;
+          response.body.should.be.a('array');
+          response.body.length.should.equal(1);
+          response.body[0].should.have.property('game_title')
+          response.body[0].should.have.property('game_image')
+          response.body[0].should.have.property('created_at');
+          response.body[0].should.have.property('updated_at');
+        })
+        done();
+      })
+    });
 });
